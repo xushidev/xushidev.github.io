6be3df2a1c36249ee6485514de56eeb152f54715
Xushi <xushi.official@gmail.com>
2025-05-16 13:09:16 +0200

    Laying out the skeleton of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { Button } from "@chakra-ui/react"
+import { Box, useColorModeValue } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Blog from "./pages/Blog"
+import Garden from "./pages/Garden"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-    <>
-      <Button>
-        hello
-      </Button>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/projects" element={ <Projects/> }/>
+        <Route path="/blog" element={ <Blog/> } />
+        <Route path="/garden" element={ <Garden/> } />
+        {/*
+          Home -> Skills, descriptions (about me) and contacts
+          Projects -> Live projects and archived projects
+          Blog -> Bloggings (Github repository)
+          Garden -> School notes and my own notes (Github Repository)
+        */} 
+      </Routes>
+    </Box>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import { Button, Container, Flex, HStack, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import React from 'react'
+import { FaRss } from 'react-icons/fa';
+import { GrProjects } from 'react-icons/gr';
+import { IoMoon } from 'react-icons/io5';
+import { LuSun } from 'react-icons/lu';
+import { TbNote } from 'react-icons/tb';
+import { Link } from 'react-router';
+
+const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    return <Container maxW={"1140px"} px={4}>
+        <Flex
+            h={16}
+            alignItems={"center"}
+            justify={"space-between"}
+            flexDir={{
+                base:"column",
+                sm:"row"
+            }}
+        >
+            <Text
+                fontSize={{ base: "22", sm: "28"}}
+                fontWeight={"bold"}
+                textTransform={"uppercase"}
+                textAlign={"center"}
+            >
+                <Link to={"/"}>Xushi</Link>
+            </Text>
+
+            <HStack spacing={2} alignItems={"center"}>
+                <Link to={"/projects"}>
+                    <Button>
+                        <GrProjects fontSize={20}/>
+                    </Button>
+                </Link>
+                <Link to={"/blog"}>
+                    <Button>
+                        <FaRss fontSize={20}/>
+                    </Button>
+                </Link>
+                <Link to={"/garden"}>
+                    <Button>
+                        <TbNote fontSize={20}/>
+                    </Button>
+                </Link>
+                {/* Button for dark mode */}
+                <Button onClick={toggleColorMode}>
+                    {colorMode === "light" ? <IoMoon/> : <LuSun fontSize={20}/>}
+                </Button>
+            </HStack>
+        </Flex>
+    </Container>;
+}
+
+export default Navbar;
